dba81605286f641f64e3520c35208fdfa9cc2bcb
Alessandro Vozza <alessandro.vozza@microsoft.com>
2021-01-27 01:31:24 +0100

    remove type

--- aks-kubenet-multipool-multisubnet/terraform/poolsubnets.tf
+++ aks-kubenet-multipool-multisubnet/terraform/poolsubnets.tf
@@ -1,8 +1,6 @@
 variable "poolsubnets" {
   description = "Subnets and pools"
-  type        = map(string)
   default = {
-
     akssubnetbase  = ["172.20.1.0/24", "Standard_B4ms", 1]
     akssubnetextra = ["172.20.2.0/24", "Standard_B4ms", 1]
   }
